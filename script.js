bdd6a200770feb14ee42ba129545f4fc87591a16
Belle <roseabelle84@gmail.com>
2022-06-16 08:29:57 -0500

    added html weather detail

---
+++ script.js
@@ -0,0 +1,8 @@
+fetch("7e37e786adcb01f704eb390ad5c6d179");
+const timeEl = document.getElementaryId("time");
+const dateE1 = document.getElementaryId("date");
+const currentWeatherItemsE1 = document.getElementById
+    ("current-weather-items");
+const timezone = document.getElementaryId("timezone");
+const countryE1 = document.getElementaryId("countryE1");
+const
